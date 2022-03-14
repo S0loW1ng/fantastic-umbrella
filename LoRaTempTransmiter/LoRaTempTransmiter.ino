40a7d36e1f31887e67cd28aa9ac4356de86ced99
retr0 <gg@gg.com>
2022-03-14 12:43:13 -0700

    Format change

--- LoRaTempTransmiter/LoRaTempTransmiter.ino
+++ LoRaTempTransmiter/LoRaTempTransmiter.ino
@@ -2,34 +2,31 @@
 #define BAND 915E6; //Parameter definitions
 String rssi = "RSSI --";
 String packSize = "--";
-String packet ;
+String packet;
 
 #include <DHT.h> // Temp Sensor Lib
 #define DHTPIN 2 // Parameter Deffinitions
 #define DHTTYPE DHT11
-
-DHT dht(DHTPIN,DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE);
 
 //Global variables
 float temperature = 99.99;
 
-
 void setup() {
-  Serial.begin(9600)
   
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
-  Heltec.display->init();
-  Heltec.display->flipScreenVertically();  
-  Heltec.display->setFont(ArialMT_Plain_10);
-  Heltec.display->drawString(0, 0, "Heltec.LoRa Initial success!");
+  Serial.begin(9600)
+
+  Heltec.begin(true /*DisplayEnable Enable*/ , true /*Heltec.Heltec.Heltec.LoRa Disable*/ , true /*Serial Enable*/ , true /*PABOOST Enable*/ , BAND /*long BAND*/ );
+  Heltec.display -> init();
+  Heltec.display -> flipScreenVertically();
+  Heltec.display -> setFont(ArialMT_Plain_10);
+  Heltec.display -> drawString(0, 0, "Heltec.LoRa Initial success!");
   delay(2000);
 
-  Heltec.display->display();
+  Heltec.display -> display();
   delay(1000);
-  Heltec.display->clear();
-  
-  
-  
+  Heltec.display -> clear();
+
 }
 
 void loop() {
@@ -40,32 +37,32 @@ void loop() {
 
 }
 
-void getReadings(){
-  
+void getReadings() {
+
   temperature = dht.readTemperature();
-  Serial.printf("Temperature: %f \n",temperature);
+  Serial.printf("Temperature: %f \n", temperature);
   delay(50);
-  
+
 }
 
-void displayReadings(){
-  
-  Heltec.display->clear();
-  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
-  Heltec.display->setFont(ArialMT_Plain_10);
-  Heltec.display->drawString(0, 0, "Temperature: ");
-  Heltec.display->drawString(90, 0, (String(temperature) + " C"));
-  Heltec.display->display();
+void displayReadings() {
+
+  Heltec.display -> clear();
+  Heltec.display -> setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display -> setFont(ArialMT_Plain_10);
+  Heltec.display -> drawString(0, 0, "Temperature: ");
+  Heltec.display -> drawString(90, 0, (String(temperature) + " C"));
+  Heltec.display -> display();
   delay(50);
-  
+
 }
 
-void sendPacket(String str){
+void sendPacket(String str) {
 
   LoRa.beginPacket();
-  LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST); //RF_PACONFIG_PASELECT_PABOOST -- LoRa single output via PABOOST, maximum output 20dBm
+  LoRa.setTxPower(14, RF_PACONFIG_PASELECT_PABOOST); //RF_PACONFIG_PASELECT_PABOOST -- LoRa single output via PABOOST, maximum output 20dBm
   LoRa.print(str);
   LoRa.endPacket();
   delay(50);
-  
+
 }
